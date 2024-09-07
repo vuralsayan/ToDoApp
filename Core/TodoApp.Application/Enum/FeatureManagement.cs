266523377dcf68a7e73b601d57f055b66e2ba05d
Vural Sayan <vuralsayan01@gmail.com>
2024-09-07 22:37:15 +0300

    Add enums for feature flags and roles, and Response<T> class

---
+++ Core/TodoApp.Application/Enum/FeatureManagement.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApp.Application.Enum
+{
+    public enum FeatureManagement
+    {
+        EnableEmailService
+    }
+}
---
+++ Core/TodoApp.Application/Enum/Roles.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApp.Application.Enum
+{
+    public enum Roles
+    {
+        Admin,
+        Moderator,
+        User
+    }
+
+    public static class Constants
+    {
+        public static readonly string Admin = Guid.NewGuid().ToString();
+        public static readonly string Moderator = Guid.NewGuid().ToString();
+        public static readonly string User = Guid.NewGuid().ToString();
+    }
+}
