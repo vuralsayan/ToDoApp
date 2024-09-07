1149e59d237ddb4404f70957ed0961e64c0c7e57
Vural Sayan <vuralsayan01@gmail.com>
2024-09-07 22:11:01 +0300

    Add authentication classes to Application layer

---
+++ Core/TodoApp.Application/Auth/AuthenticationRequest.cs
@@ -0,0 +1,9 @@
+﻿#nullable disable
+namespace TodoApp.Application.Auth
+{
+    public class AuthenticationRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Core/TodoApp.Application/Auth/ForgotPasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Application.Auth
+{
+    public class ForgotPasswordRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
